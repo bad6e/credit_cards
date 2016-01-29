129b7641a54bc8b26db855ea5e77e3d916d04446
Bret Doucette <doucette.bret@gmail.com>
2016-01-29 12:01:40 -0700

    Added Citext for case insensitivity

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160129164240) do
+ActiveRecord::Schema.define(version: 20160129190039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "citext"
 
   create_table "cards", force: :cascade do |t|
-    t.string   "name"
+    t.citext   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "annual_fee"
