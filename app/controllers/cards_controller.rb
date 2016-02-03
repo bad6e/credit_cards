85d002c9c0592498940a5e94ef86573321204881
Bret Doucette <doucette.bret@gmail.com>
2016-02-03 14:07:22 -0700

    Moved logic into presenter class and out of cards controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,8 +1,5 @@
 class CardsController < ApplicationController
   def show
-    @card = Card.find(params[:id])
-    @category_name = Card.find(params[:id]).parse_name
-    category = Card.find(params[:id]).categories
-    @featured_cards = category.first.cards.limit(4).order("RANDOM()")
+    @card = CardPresenter.new(params[:id])
   end
 end
---
+++ app/presenters/card_presenter.rb
@@ -0,0 +1,26 @@
+class CardPresenter
+
+  attr_reader :card_id
+
+  def initialize(card_id)
+    @card_id = card_id
+  end
+
+  def card_name
+    Card.find(card_id)
+  end
+
+  def category_name
+    Card.find(card_id).parse_name
+  end
+
+  def featured_cards
+    categories.first.cards.limit(4).order("RANDOM()")
+  end
+
+  private
+
+  def categories
+    Card.find(card_id).categories
+  end
+end
