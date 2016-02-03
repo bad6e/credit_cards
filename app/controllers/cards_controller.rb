e55ddf2b8caf7fdfaeff16aa700cdab6c23106f1
Bret Doucette <doucette.bret@gmail.com>
2016-02-03 12:17:28 -0700

    Display related card category names

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,7 +1,7 @@
 class CardsController < ApplicationController
   def show
     @card = Card.find(params[:id])
-
+    @related_card = Card.find(params[:id]).parse_name
     category = Card.find(params[:id]).categories
     @featured_cards = category.first.cards.limit(4).order("RANDOM()")
   end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,9 @@
 class Card < ActiveRecord::Base
   has_many :categorizings
   has_many :categories, through: :categorizings
+
+  def parse_name
+    name = self.categories.first.name
+    display_name = name.gsub("-", " ").titleize
+  end
 end
