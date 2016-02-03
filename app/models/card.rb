c30b6d37e3212249fc755ffcdd3be05f3b19ac73
Bret Doucette <doucette.bret@gmail.com>
2016-02-03 14:29:51 -0700

    Added card model tests

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,6 +2,13 @@ class Card < ActiveRecord::Base
   has_many :categorizings
   has_many :categories, through: :categorizings
 
+  validates :name, presence: true, uniqueness: true
+  validates :annual_fee, presence: true, uniqueness: true
+  validates :information, presence: true
+  validates :apr, presence: true, uniqueness: true
+  validates :intro_rate, presence: true, uniqueness: true
+  validates :image_link, presence: true, uniqueness: true
+
   def parse_name
     name = self.categories.first.name
     display_name = name.gsub("-", " ").titleize
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:card_one) {
+    Card.create(name: "Southwest Airlines Premier",
+                annual_fee: "$99",
+                information: ["Bullet A", "Bullet B"],
+                apr: "12%",
+                intro_rate: "N/A",
+                image_link: "www.test-premier.com")
+  }
+
+  let(:card_two) {
+    Card.create(name: "Southwest Airlines Plus",
+                annual_fee: "$59",
+                information: ["Bullet C", "Bullet D"],
+                apr: "12%",
+                intro_rate: "N/A",
+                image_link: "www.test-plus.com")
+  }
+
+  it "is valid" do
+    expect(card_one).to be_valid
+  end
+
+  it "is invalid without a name" do
+    card_one.name = nil
+    expect(card_one).to be_invalid
+  end
+
+  it "is invalid without an annual fee" do
+    card_one.annual_fee = nil
+    expect(card_one).to be_invalid
+  end
+
+  it "is invalid without a card information" do
+    card_one.information = nil
+    expect(card_one).to be_invalid
+  end
+
+  it "is invalid without an apr rate" do
+    card_one.apr = nil
+    expect(card_one).to be_invalid
+  end
+
+  it "is invalid without an intro rate" do
+    card_one.intro_rate = nil
+    expect(card_one).to be_invalid
+  end
+
+  it "is invalid without an image url" do
+    card_one.image_link = nil
+    expect(card_one).to be_invalid
+  end
 end
