cde6fe46811f2387b37b5449bff47e44b1bf6d85
Bret Doucette <doucette.bret@gmail.com>
2016-02-13 12:33:22 -0700

    Added admin can log in spec

---
+++ spec/features/admin/admin_can_login_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+feature "admin" do
+  scenario "admin can log in" do
+    visit admin_login_path
+
+    user = User.create(first_name: "Bret",
+                last_name: "Doucette",
+                email: "test@test.com",
+                password: "password",
+                role: 1)
+
+    fill_in "user[email]", with: user.email
+    fill_in "user[password]", with: user.password
+    click_on "Login"
+
+    expect(current_path).to eq(admin_cards_path)
+  end
+end
