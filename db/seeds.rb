a9ac349c0070dd0b9e9141aa51f8130e4c9bbc85
Bret Doucette <doucette.bret@gmail.com>
2016-02-18 11:39:33 -0700

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
-Category.create(name: 'airline')
-Category.create(name: 'travel')
-Category.create(name: 'business-rewards')
-Category.create(name: 'hotel')
-Category.create(name: 'cash-back')
-Category.create(name: 'other')
+Category.create(name: 'airline', slug: 'airline')
+Category.create(name: 'travel', slug: 'travel')
+Category.create(name: 'business-rewards', slug: 'business-rewards')
+Category.create(name: 'hotel', slug: 'hotel')
+Category.create(name: 'cash-back', slug: 'cash-back')
+Category.create(name: 'other', slug: 'other')
+Category.create(name: "bret's-favorite-cards", slug: "brets-favorite-cards")
+
+User.create(first_name: "Bret",
+            last_name: "Doucette",
+            email: "doucette.bret@gmail.com",
+            password: "Kt4ybbAdmin",
+            role: 1)
