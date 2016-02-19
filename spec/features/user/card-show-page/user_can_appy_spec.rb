08335d279bcc62f2aacf42f30ce5aa5f86de9b14
Bret Doucette <doucette.bret@gmail.com>
2016-02-19 14:15:58 -0700

    Tests for apply here button

---
+++ spec/features/user/card-show-page/user_can_appy_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+feature "user" do
+  include_context "features"
+
+  scenario "user can see apply here button for card in 'Bret's Favorite Cards" do
+    set_card_categories
+    admin_edit
+    fill_in_reward_information
+
+    card_one.categories << Category.find(category_five.id)
+    visit card_path(card_one)
+
+    within(".apply-here") do
+      expect(page).to have_button('Apply Here')
+    end
+  end
+end
