a55026ec6a1121c0bcca37768803b962f9fb3180
Bret Doucette <doucette.bret@gmail.com>
2016-02-19 14:17:22 -0700

    Added test for not apply here

--- spec/features/user/card-show-page/user_can_appy_spec.rb
+++ spec/features/user/card-show-page/user_can_appy_spec.rb
@@ -15,4 +15,13 @@ feature "user" do
       expect(page).to have_button('Apply Here')
     end
   end
+
+  scenario "user can NOT see apply here button for card NOT in 'Bret's Favorite Cards" do
+    set_card_categories
+    admin_edit
+    fill_in_reward_information
+
+    visit card_path(card_one)
+    expect(page).to_not have_button('Apply Here')
+  end
 end
