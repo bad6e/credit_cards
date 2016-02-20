25bdcf3e9fce033b1b156e8645aed88bac0926f9
Bret Doucette <doucette.bret@gmail.com>
2016-02-20 16:42:39 -0700

    Added feature test for searching

--- spec/features/user/navbar/user_can_search_for_cards_spec.rb
+++ spec/features/user/navbar/user_can_search_for_cards_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 feature "navbar" do
   include_context "features"
 
-  scenario "user can search for a specific card in the search bar" do
+  scenario "user can search for a specific card in the search bar", js: true do
     set_card_categories
     admin_edit
     fill_in_reward_information
@@ -12,11 +12,15 @@ feature "navbar" do
 
     within("#search-form") do
       fill_in "search", with: "South"
-      click_on "Go!"
+      find('#search-button').click
     end
 
-    expect(page).to have_content("Your Search Results Returned 1 Card")
+    expect(page).to have_content("Your Search Results Returned 2 Cards")
 
+    within(".search-results") do
+      expect(page).to have_content("Southwest Airlines Premier")
+      expect(page).to have_content("Southwest Airlines Plus")
+    end
 
   end
 
@@ -37,13 +41,4 @@ feature "navbar" do
       expect(page).to have_content("Southwest Airlines Plus")
     end
   end
-
-  # scenario "user can NOT see apply here button for card NOT in 'Bret's Favorite Cards" do
-  #   set_card_categories
-  #   admin_edit
-  #   fill_in_reward_information
-
-  #   visit card_path(card_one)
-  #   expect(page).to_not have_button('Apply Here')
-  # end
 end
