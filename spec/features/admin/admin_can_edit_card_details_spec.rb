78064fafa499a91ef9c1257c4dde0cd7d9e45a1a
Bret Doucette <doucette.bret@gmail.com>
2016-02-20 10:41:05 -0700

    Added test to make sure page ul split correctly

--- spec/features/admin/admin_can_edit_card_details_spec.rb
+++ spec/features/admin/admin_can_edit_card_details_spec.rb
@@ -41,7 +41,7 @@ feature "admin" do
       find_field('card[apr]').value.should eq "12.99%"
       find_field('card[intro_rate]').value.should eq "N/A"
       find_field('card[image_link]').value.should eq "www.test.com"
-      find_field('card[information]').value.should eq "This is the best card ever.  I really like it.  Fact."
+      find_field('card[information]').value.should eq "This is the best card ever. I really like it. Fact.."
     end
 
     visit card_path(card_one)
@@ -111,4 +111,34 @@ feature "admin" do
     expect(card_one.categories.include?(Category.find_by(name: "travel"))).to eq(true)
     expect(card_one.categories.include?(Category.find_by(name: "hotel"))).to eq(true)
   end
- end
+
+  scenario "admin can edit card information and have the information formatted correctly" do
+    admin_edit
+
+    fill_in "card[name]", with: "Test Name1"
+    fill_in "card[annual_fee]", with: "$99"
+    fill_in "card[apr]", with: "12.99%"
+    fill_in "card[intro_rate]", with: "N/A"
+    fill_in "card[image_link]", with: "www.test.com"
+    fill_in "card[information]", with: "This card has 16.24%. The price on this card is 3.00. I..like this card."
+    select "airline", :from => "card[categories][]"
+    select "travel", :from => "card[categories][]"
+    select "cash-back", :from => "card[categories][]"
+    click_on "Submit Information"
+
+    expect(current_path).to eq(admin_cards_path)
+
+    visit card_path(card_one)
+    expect(page).to have_content("Test Name1")
+    expect(page).to have_content("$99")
+    expect(page).to have_content("12.99%")
+    expect(page).to have_content("N/A")
+
+    within(".card-description") do
+      expect(page).to have_css('ul')
+      expect(page).to have_content("This card has 16.24%.")
+      expect(page).to have_content("The price on this card is 3.00.")
+      expect(page).to have_content("I..like this card.")
+    end
+  end
+end
