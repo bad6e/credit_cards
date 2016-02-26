f83bf6cab53d629e80ca960e9f9de0d8cd16607f
Bret Doucette <doucette.bret@gmail.com>
2016-02-26 10:55:24 -0700

    Formatted controller to take csvs and dates

--- app/controllers/admin/imports_controller.rb
+++ app/controllers/admin/imports_controller.rb
@@ -1,7 +1,12 @@
 class Admin::ImportsController < ApplicationController
   def import
-    Parser.parse_csv(params[:file])
-    flash[:success] = "CSV successfully imported!"
-    redirect_to admin_cards_path
+    begin
+      Parser.parse_csv(params[:file], params[:date])
+      flash[:success] = "CSV successfully imported!"
+      redirect_to admin_cards_path
+    rescue
+      flash[:errors] = "Improper Format"
+      redirect_to admin_cards_path
+    end
   end
 end
