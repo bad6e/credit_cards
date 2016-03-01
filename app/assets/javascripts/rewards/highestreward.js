f2c2e9737bc6668c2beee00466a8041e7ea8213a
Bret Doucette <doucette.bret@gmail.com>
2016-03-01 14:04:37 -0700

    Pushing debugger

--- app/assets/javascripts/rewards/highestreward.js
+++ app/assets/javascripts/rewards/highestreward.js
@@ -9,6 +9,7 @@ function checkIfCardHasRewards(response) {
 function calculateHighestReward(response) {
   var currentBonus = response[0].amount
   var maxReward = _.maxBy(response, function(o){return o.amount;}).amount;
+  debugger
   if (maxReward === currentBonus) {
     $('#good-time').text("Yes");
   } else {
