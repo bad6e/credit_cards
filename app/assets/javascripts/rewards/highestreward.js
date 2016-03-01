9781080e940b0e74fff580ca11c282cc47fcbd0c
Bret Doucette <doucette.bret@gmail.com>
2016-03-01 13:36:23 -0700

    Fixed bug

--- app/assets/javascripts/rewards/highestreward.js
+++ app/assets/javascripts/rewards/highestreward.js
@@ -9,8 +9,7 @@ function checkIfCardHasRewards(response) {
 function calculateHighestReward(response) {
   var currentBonus = response[0].amount
   var maxReward = _.maxBy(response, function(o){return o.amount;}).amount;
-  var ratio = maxReward/currentBonus
-  if (ratio <=1) {
+  if (maxReward === currentBonus) {
     $('#good-time').text("Yes");
   } else {
     $('#good-time').text("No").css({ 'color': 'red'});
