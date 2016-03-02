a65d398dd980b4b9fc8e5e5ca5fdca2256da75df
Bret Doucette <doucette.bret@gmail.com>
2016-03-02 15:00:53 -0700

    Add in categories

---
+++ app/assets/javascripts/rewards/formatapply.js
@@ -0,0 +1,16 @@
+function formatColors() {
+  $( ".best-offer-color" ).each(function( index, element ) {
+    if ($(element).text() === "n/a") {
+      $( ".loader-small-category" ).fadeOut(700)
+      $(element).show();
+      $(element).css({ 'color': 'black'});
+    } else if ($(element).text() === "yes") {
+       $(element).show();
+      $(element).css({ 'color': 'green'});
+    } else if ($(element).text() === "no") {
+       $(element).show();
+      $(element).css({ 'color': 'red'});
+    }
+  })
+}
+
