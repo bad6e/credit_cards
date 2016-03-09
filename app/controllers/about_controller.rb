0cf5360a3062dcb7845a65ddbec737c075612808
Bret Doucette <doucette.bret@gmail.com>
2016-03-09 14:03:10 -0700

    Finished about page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -2,5 +2,6 @@ class AboutController < ApplicationController
 
   def index
     @linked_card = Card.find_by(name: "Chase Sapphire Preferred® Card")
+    @best_cards  = Category.find_by(name: "best-credit-card-offers")
   end
 end
