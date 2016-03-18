95dadcd6c4fa77af352052333de049526c48fe14
Bret Doucette <doucette.bret@gmail.com>
2016-03-17 18:23:50 -0600

    Fixed button name

--- app/assets/javascripts/components/_categoryCards.jsx
+++ app/assets/javascripts/components/_categoryCards.jsx
@@ -3,7 +3,7 @@ var SortByName = React.createClass({
     return (
       <div className="sort-by-section clearfix box" id="sorter-box">
         <h4 className="sort-by-title block-sm">Sort Results By:</h4>
-        <button className="button btn-small" id="sort-button" onClick={this.props.sortCardsByName}>Name</button>
+        <button className="button btn-small" id="sort-button" onClick={this.props.sortCardsByName}>Card Name</button>
         <button className="button btn-small" id="sort-button" onClick={this.props.sortCardsByBestOffer}>Apply Now?</button>
         <button className="button btn-small" id="sort-button" onClick={this.props.sortCardsByAmount}>Current Bonus</button>
       </div>
