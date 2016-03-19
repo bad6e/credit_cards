0ec6658b98e9ea03843c29f6440f7e45888d3053
Bret Doucette <doucette.bret@gmail.com>
2016-03-19 10:07:56 -0600

    Configured facebook to give me email WHAHAHHH

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth_hash)
     user                 = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
     user.name            = auth_hash['info']['name']
-    user.email           = SecureRandom.urlsafe_base64
+    user.email           = auth_hash['info']['email']
     user.password_digest = SecureRandom.urlsafe_base64
     user.location        = auth_hash['info']['location']
     user.image_url       = auth_hash['info']['image']
