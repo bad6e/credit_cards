785e1d043b50b009b0f2130b1fcda04f9be6da58
Bret Doucette <doucette.bret@gmail.com>
2016-03-26 14:30:08 -0600

    Added in an after_update

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -13,4 +13,10 @@ class Reward < ActiveRecord::Base
   validates :length_of_time,
     presence: true
 
+  after_create :set_cent_and_dollar_value
+
+  def set_cent_and_dollar_value
+    most_recent_reward = Reward.where('card_id = ? AND cent_value > ?', 40, 0).order(record_date: :desc).first
+    self.update(cent_value: most_recent_reward.cent_value, dollar_amount: most_recent_reward.cent_value * most_recent_reward.amount)
+  end
 end
