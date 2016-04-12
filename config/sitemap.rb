ec24fabe89d9d76c5541faaa3d6e482c14edcb8c
Bret Doucette <doucette.bret@gmail.com>
2016-04-12 11:08:45 -0600

    Updated sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -4,7 +4,6 @@ SitemapGenerator::Sitemap.public_path = 'tmp/sitemaps/'
 SitemapGenerator::Sitemap.create do
   add root_path
   add about_index_path
-  add cards_path, changefreq: 'monthly'
   add categories_path
 
   Card.find_each do |card|
