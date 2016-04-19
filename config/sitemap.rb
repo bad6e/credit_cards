ce75e2637a357495cf79d8979860f9d551236346
Bret Doucette <doucette.bret@gmail.com>
2016-04-19 12:32:34 -0600

    updated sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -11,6 +11,10 @@ SitemapGenerator::Sitemap.create do
     add card_path(card), lastmod: card.updated_at
   end
 
+  Blog.find_each do |blog|
+    add blog_path(blog), lastmod: card.updated_at
+  end
+
   Category.find_each do |category|
     add category_path(category.slug), lastmod: category.updated_at
   end
