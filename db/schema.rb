44b0559cdc69070a8d8064563c40735dd35b46a7
Bret Doucette <doucette.bret@gmail.com>
2016-04-20 10:45:46 -0600

    Added image url to card

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420153015) do
+ActiveRecord::Schema.define(version: 20160420164154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20160420153015) do
     t.string   "best_offer"
     t.string   "flyer_talk_link"
     t.string   "point_type"
+    t.string   "image_url"
   end
 
   create_table "categories", force: :cascade do |t|
