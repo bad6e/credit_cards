1f363b9c0a9f5bbae7351838a387fef7327938b6
Bret Doucette <doucette.bret@gmail.com>
2016-04-21 13:40:05 -0600

    Added react component

---
+++ app/assets/javascripts/components/_relatedBlogs.jsx
@@ -0,0 +1,29 @@
+var RelatedBlogs = React.createClass({
+  render : function() {
+    return (
+      <div className="col-sm-4">
+        <article className="box">
+          <figure>
+            <a title="" href=""><img alt="" src=""/></a>
+              <figcaption>
+                <h3 className="caption-title">20</h3>
+                <span>APR</span>
+              </figcaption>
+          </figure>
+          <div className="details details-text-box">
+            <div className="icon-box style11">
+              <div className="icon-wrapper">
+                <i className="soap-icon-plane-right takeoff-effect circle"></i>
+              </div>
+              <div className="details">
+                <h4 className="box-title">Title<small>Posted By: Bret</small></h4>
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+    )
+  }
+});
+
+
