09dad7a3d516ba85da211e0d93b064385b69d9db
Bret Doucette <doucette.bret@gmail.com>
2016-04-28 15:01:30 -0600

    Added published t/f in db

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ Rails.application.routes.draw do
     get 'imports/index'
     resources :cards, only: [:new, :create, :edit, :update, :index]
     resources :rewards, only: [:new, :create, :edit, :update, :index, :destroy]
-    resources :blogs, only: [:index, :new, :create, :edit, :update, :index, :destroy]
+    resources :blogs
     resources :imports do
       collection { post :import}
     end
