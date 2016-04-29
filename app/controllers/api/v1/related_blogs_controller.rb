86193c59a96c852b784d746d8567101771cbdb08
Bret Doucette <doucette.bret@gmail.com>
2016-04-28 20:59:53 -0600

    Fixed bug

--- app/controllers/api/v1/related_blogs_controller.rb
+++ app/controllers/api/v1/related_blogs_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::RelatedBlogsController < ApplicationController
   respond_to :json
 
   def show
-    @related_blogs = Card.find(params[:id]).blogs.order('created_at DESC')
+    @related_blogs = Card.find(params[:id]).blogs.order('created_at DESC').where(published: true)
     respond_with @related_blogs
   end
 end
