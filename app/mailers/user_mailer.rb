3b6aff38e53424f5d9b4310917a65e3188b101a3
Bret Doucette <doucette.bret@gmail.com>
2016-05-04 12:33:44 -0600

    Updated email test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @blogs = Blog.order('created_at DESC').where(published: true).limit(3)
-    mail(to: @user.email, subject: "Welcome to Too Many Miles!", reply_to: "bret@toomanymiles.com")
+    mail(to: 'doucette.bret@gmail.com', subject: "Welcome to Too Many Miles!", reply_to: "bret@toomanymiles.com")
   end
 end
