f390561fd5dbc2b3b2b9f13b9c7c7aa896ca7eb9
Bret Doucette <doucette.bret@gmail.com>
2016-05-04 12:18:08 -0600

    Updated localhost

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
 
    # Mailer Settings
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'heroku.com'}
+  config.action_mailer.default_url_options = { host: 'http://www.toomanymiles.com/'}
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
