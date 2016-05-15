9316bc7dae6696a4443b8eee51e95147847eb823
Bret Doucette <doucette.bret@gmail.com>
2016-05-14 18:15:14 -0600

    opened params

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -8,7 +8,7 @@ class Blog < ActiveRecord::Base
   validates :meta_title,
     presence: true,
     uniqueness: true,
-    length: { maximum: 60}
+    length: { maximum: 75}
 
   validates :meta_description,
     presence: true,
