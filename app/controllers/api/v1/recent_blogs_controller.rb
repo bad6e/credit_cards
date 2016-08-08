0aecad2f886e9903ed32e57008fdfafa11c67249
Bret Doucette <doucette.bret@gmail.com>
2016-08-08 11:46:09 -0600

    Fixed stuff

--- app/controllers/api/v1/recent_blogs_controller.rb
+++ app/controllers/api/v1/recent_blogs_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::RecentBlogsController < ApplicationController
    respond_to :json
 
   def index
-    recent_blogs = Blog.order(created_at: :desc).where(published: true).limit(6)
+    recent_blogs = Blog.order(created_at: :desc).where(published: true).limit(4)
     respond_with recent_blogs
   end
 end
