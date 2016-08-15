ae2c2bc21b4cd4ba97c3352fc3201b47274bb98c
Bret Doucette <doucette.bret@gmail.com>
2016-08-15 12:15:50 -0400

    All the things

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  after_action :allow_stumble_upon_iframe
 
   include SessionsHelper
   include ApplicationHelper
@@ -14,5 +15,9 @@ class ApplicationController < ActionController::Base
 
   def no_index_no_follow
     @meta_robots_instructions = "noindex, nofollow"
+  end
+
+  def allow_stumble_upon_iframe
+    response.headers['X-Frame-Options'] = 'ALLOW-FROM http://www.stumbleupon.com/'
   end
 end
