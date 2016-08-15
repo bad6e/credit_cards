1165ed0d782658fc9682ae710475c4b20809502c
Bret Doucette <doucette.bret@gmail.com>
2016-08-15 12:05:51 -0400

    Attempt to get stumble to work

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,9 +1,14 @@
 class BlogsController < ApplicationController
+  after_action :allow_stumble_upon_iframe
   def index
   end
 
   def show
     @blog = Blog.where(slug: params[:id], published: true).first
     @related_cards = Blog.friendly.find(params['id']).cards.limit(4).order("RANDOM()")
+  end
+
+  def allow_stumble_upon_iframe
+    response.headers['X-Frame-Options'] = 'ALLOW-FROM http://www.stumbleupon.com/'
   end
 end
