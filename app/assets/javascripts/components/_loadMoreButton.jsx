cd0780c524cb0f02266f3c484f3e284ce57d168b
Bret Doucette <doucette.bret@gmail.com>
2016-11-25 10:46:25 -0500

    Deleted old folder

--- app/assets/javascripts/components/_loadMoreButton.jsx
+++
@@ -1,7 +0,0 @@
-var LoadMoreButton = React.createClass({
-  render : function() {
-    return (
-      <button className ="button btn-large full-width" onClick={this.props.loadMoreCards}>LOAD MORE POSTS</button>
-    )
-  }
-});
--- app/assets/javascripts/components/transfer-partners/_partner.jsx
+++
@@ -1,9 +0,0 @@
-var Partner = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <li>{this.props.partners.name}</li>
-      </div>
-    )
-  }
-});
