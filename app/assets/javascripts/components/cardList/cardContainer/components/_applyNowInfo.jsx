c5e36134f33afbcd62795a5f653f0fe08e5013f8
Bret Doucette <doucette.bret@gmail.com>
2016-12-14 16:09:01 -0700

    test

--- app/assets/javascripts/components/cardList/cardContainer/components/_applyNowInfo.jsx
+++ app/assets/javascripts/components/cardList/cardContainer/components/_applyNowInfo.jsx
@@ -43,14 +43,12 @@ var ApplyNowInfo = React.createClass({
 
   displayNormalBonusInfo: function (reward, details) {
     return (
-      <a href="javascript:void(0);">
-        <div className="tooltop-apply-now">
+        <a className="tooltop-apply-now">
           <span className="tooltop-apply-now-text">
             <p>This card's current bonus is <strong>{this.props.numberWithCommas(reward)} {details.point_type}.</strong></p>
             <p>Based on this card's past bonuses, we believe this card's <strong>current</strong> bonus is a <strong><span style={this.props.determineBestOfferColor(details.best_offer)}>{this.determineDealStatus(details.best_offer)}</span></strong> deal.</p>
           </span>
           <h6 className="apply-now-title question-icon">Apply Now</h6>
-        </div>
       </a>
     )
   },
