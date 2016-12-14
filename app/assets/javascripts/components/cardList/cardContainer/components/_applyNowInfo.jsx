f9da1a33a258542c9f3005afca2675816453bce8
Bret Doucette <doucette.bret@gmail.com>
2016-12-14 15:28:10 -0700

    EH

--- app/assets/javascripts/components/cardList/cardContainer/components/_applyNowInfo.jsx
+++ app/assets/javascripts/components/cardList/cardContainer/components/_applyNowInfo.jsx
@@ -43,7 +43,7 @@ var ApplyNowInfo = React.createClass({
 
   displayNormalBonusInfo: function (reward, details) {
     return (
-      <a href="javascript:void(0);">
+      <a onclick="">
       <div className="tooltop-apply-now">
         <span className="tooltop-apply-now-text">
           <p>This card's current bonus is <strong>{this.props.numberWithCommas(reward)} {details.point_type}.</strong></p>
