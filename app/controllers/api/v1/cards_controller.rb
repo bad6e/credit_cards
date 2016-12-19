4e48648f3a7d9417bb21432a3affa457aa90ba6b
Bret Doucette <doucette.bret@gmail.com>
2016-12-19 13:51:27 -0700

    Fun caching

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -13,11 +13,11 @@ class Api::V1::CardsController < ApplicationController
   end
 
   def names
-    a = []
-    cards  = Card.all.pluck(:id, :name)
+    card_names = []
+    cards = Card.card_names
     cards.each do |card|
-      a << {id: card[0], name: card[1]}
+      card_names << {id: card[0], name: card[1]}
     end
-    respond_with a
+    respond_with card_names
   end
 end
