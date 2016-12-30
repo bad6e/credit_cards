162182b99fc2e2ea4f9f74e78b4877eca71238a7
Bret Doucette <doucette.bret@gmail.com>
2016-12-30 15:22:41 -0700

    Added in blog mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,11 @@ class UserMailer < ApplicationMailer
     @blogs = Blog.order('created_at DESC').where(published: true).limit(3)
     mail(to: @user.email, subject: "Welcome to Too Many Miles!", reply_to: "bret@toomanymiles.com")
   end
+
+  def blog_email(blog, user, recent_blogs)
+    @user = user
+    @blog = blog
+    @recent_blogs = recent_blogs
+    mail(to: @user.email, subject: @blog.meta_title, reply_to: "bret@toomanymiles.com")
+  end
 end
