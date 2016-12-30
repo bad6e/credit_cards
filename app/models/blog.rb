ec667bb7d6ae6f02bf6186480125574338ec9b35
Bret Doucette <doucette.bret@gmail.com>
2016-12-30 15:22:56 -0700

    Added in blog logic

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -18,4 +18,12 @@ class Blog < ActiveRecord::Base
   def self.parameterize_slug(blog)
     blog.update(slug: blog.meta_title.parameterize)
   end
+
+  def self.recent_blogs(excluded_blog, number)
+    where.not(id: excluded_blog.id).order('created_at DESC').limit(number)
+  end
+
+  def self.send_blog_email(blog, user)
+    UserMailer.delay.blog_email(blog, user,  Blog.recent_blogs(blog, 3))
+  end
 end
