6f2f9ae3d93261a721e8319407b57f241382c96c
Bret Doucette <doucette.bret@gmail.com>
2017-01-12 16:31:21 -0700

    YOLO

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,5 @@
 class Card < ActiveRecord::Base
-  has_many :rewards, dependent: :destroy
+  has_many :rewards, -> { order(:record_date) }
 
   has_many :categorizings
   has_many :categories, through: :categorizings
