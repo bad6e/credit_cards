ad4989ed29d2eb95d81624b603ba6b60781bdfbe
Bret Doucette <doucette.bret@gmail.com>
2017-02-01 16:02:42 -0700

    Updated serializers for card categories

---
+++ app/serializers/api/v1/category_card_serializer.rb
@@ -0,0 +1,27 @@
+class Api::V1::CategoryCardSerializer < ActiveModel::Serializer
+  attributes :id,
+             :name,
+             :annual_fee,
+             :apr,
+             :intro_rate,
+             :image_link,
+             :best_offer,
+             :credit_score,
+             :point_type
+
+  has_many :rewards
+  has_many :favorite_card_ids
+  has_one :search_term
+
+  def rewards
+    object.rewards.order('record_date DESC')
+  end
+
+  def favorite_card_ids
+    current_user ? current_user.cards.pluck(:id) : []
+  end
+
+  def search_term
+    @instance_options[:search_term]
+  end
+end
