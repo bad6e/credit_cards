9e8385d883435cfe98577dfaae85c93efb450df6
Bret Doucette <doucette.bret@gmail.com>
2017-02-01 16:14:53 -0700

    Updated search controller and serializer

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -5,20 +5,17 @@ class Api::V1::CardsController < ApplicationController
   def index
     if params[:search]
       cards = Card.search(params[:search]).order("created_at DESC")
-      response = { cards: cards, search_term: params[:search] }
-      render json: response, include: ['rewards']
+      respond_with cards,
+                   each_serializer: Api::V1::SearchSerializer,
+                   search_term: params[:search]
     else
-      cards = Card.all.order('created_at DESC')
+      cards = Card.all.order("created_at DESC")
       respond_with cards
     end
   end
 
-  def names
-    card_names = []
+  def card_names
     cards = Card.card_names
-    cards.each do |card|
-      card_names << { id: card[0], name: card[1] }
-    end
-    respond_with card_names
+    respond_with cards
   end
 end
