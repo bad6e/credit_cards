8776f395f291a4ec323a5bbb6fc6df0205ce85fc
Bret Doucette <doucette.bret@gmail.com>
2017-02-01 15:59:03 -0700

    Updated categories controller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,25 +1,26 @@
 class Api::V1::CategoriesController < ApplicationController
   before_action :set_cache_headers!, only: [:show]
+  before_action :find_category, only: [:show]
   respond_to :json
 
   def show
-    cards = Category.find_by(slug: category_params[:id]).cards
-    response = { cards: cards, search_term: nil, favorite_card_ids: current_user_favorite_cards }
-    render json: response, include: ['rewards']
+    cards = find_category.cards
+    respond_with cards,
+                 each_serializer: Api::V1::CategoryCardSerializer,
+                 search_term: nil
   end
 
   private
 
-    def current_user_favorite_cards
-      if current_user
-        current_user.cards.pluck(:id)
-      else
-        []
-      end
-    end
+  def current_user_favorite_cards
+    current_user ? current_user.cards.pluck(:id) : []
+  end
 
-    def category_params
-      params.permit(:id)
-    end
+  def category_params
+    params.permit(:id)
+  end
+
+  def find_category
+    @_category ||= Category.find_by(slug: category_params[:id])
+  end
 end
-
