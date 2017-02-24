6b247a85d5e7ddd1d86a8781b0e84dbb1937807f
Bret Doucette <doucette.bret@gmail.com>
2017-02-23 20:16:42 -0700

    Fixed search boom

--- app/models/card.rb
+++ app/models/card.rb
@@ -34,6 +34,8 @@ class Card < ActiveRecord::Base
   self.per_page = 10
 
   scope :find_cards_that_are_in_multiple_categories, ->(category_names) { where(id: Card.cards_with_multiple_categories(category_names)) }
+  scope :exclude_cash_and_other_cards, -> { joins(:categories).where.not({categories: {id: [5,6]}}) }
+  scope :name_like, ->(params) { where("cards.name LIKE ?", "%#{params}%") }
 
   def self.cards_with_multiple_categories(category_names)
     joins(:categories)
@@ -49,12 +51,11 @@ class Card < ActiveRecord::Base
   end
 
   def self.search(params)
-    where("name LIKE ?", "%#{params}%")
+    exclude_cash_and_other_cards.name_like(params).uniq
   end
 
   def self.card_names
-    cards =  Card.joins(:categories).where.not({categories: {id: [5,6]}})
-    cards.pluck(:id, :name).uniq
+    exclude_cash_and_other_cards.pluck(:id, :name).uniq
   end
 end
 
